79ee0f400a8a5e19a729e4e0d77d0f6365a98d36
rjablon03 <secrets_crasser.0l@icloud.com>
2025-02-08 22:27:45 -0500

    Started building models

---
+++ src/Stock.java
@@ -0,0 +1,20 @@
+public class Stock {
+    private String tickerSymbol;
+    private String closeValue;
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public void setTickerStmbol(String tickerSymbol) {
+        this.tickerSymbol = tickerSymbol;
+    }
+
+    public String getCloseValue() {
+        return closeValue;
+    }
+
+    public void setCloseValue(String closeValue) {
+        this.closeValue = closeValue;
+    }
+}
---
+++ src/StockList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class StockList {
+    private ArrayList<Stock> stocks = new ArrayList<Stock>();
+
+    public void buildStockList() {
+        int x = 9; //Build this out next
+    }
+
+    public ArrayList<Stock> getStocksList() {
+        return stocks;
+    }
+}
